3b5181efa5f657626720f7a402fb13c8fd11bdc7
meruvia <gmeruviarojas@gmail.com>
2020-03-19 20:37:08 -0400

    JSON parametrizacion y animacion de about

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nonbre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
+
